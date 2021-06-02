affe51ef5b87e8392bb2eb9e4b7b034ded7fbcd9
baxwru2 <85213182+baxwru2@users.noreply.github.com>
2021-06-02 11:16:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
   backend "remote" {
-    organization = "Killi2"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "killi2"
+      name = "gh-actions-demo"
     }
   }
 }
